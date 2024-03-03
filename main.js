67b1f60185c4dba365ae06a88e76922ba5c4e70e
NordikE <erik.b10.kodehode@gmail.com>
2024-03-03 20:18:29 +0100

    .

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const fetchAlbumData = async () => {
     const albumURL = "https://api.discogs.com/releases/28742893";
     const response = await fetch(albumURL);
     const data = await response.json();
+    console.log("Fetched album data:", data);
     return data;
   } catch (error) {
     console.error("ERROR FETCHING ALBUM DATA:", error);
@@ -16,6 +17,10 @@ const displayAlbumInfo = async () => {
   const albumData = await fetchAlbumData();
   if (!albumData) return;
 
+  console.log("Band Name:", albumData.artists[0].name);
+  console.log("Album Year:", albumData.year);
+  console.log("Label:", albumData.labels[0].name);
+
   // BAND NAME IN HEADER
   document.getElementById("bandName").textContent = albumData.artists[0].name;
 
@@ -27,17 +32,28 @@ const displayAlbumInfo = async () => {
 
   // ALBUM DETAILS
   const albumDetails = document.getElementById("albumDetails");
+
+  // LABEL LINK
+  const labelLinks = albumData.labels
+    .map((label) => {
+      const discogsURI = label.resource_url
+        .replace("api.", "")
+        .replace("/releases", "");
+      return `<a href="${discogsURI}" target="_blank">${label.name}</a>`;
+    })
+    .join(", ");
+
   albumDetails.innerHTML = `
-    <p><strong>Album Name:</strong> <a href="${albumData.uri}" target="_blank">${albumData.title}</a></p>
-    <p><strong>Year:</strong> ${albumData.year}</p>
-    <p><strong>Label:</strong> ${albumData.labels[0].name}</p>
+    <h2><strong>Album Name:</strong> <a href="${albumData.uri}" target="_blank">${albumData.title}</a></h2>
+    <h3><strong>Year:</strong> ${albumData.year}</h3>
+    <h3><strong>Label:</strong> ${labelLinks}</h3>
   `;
 
   // UPDATE TRACKLIST
   const tracklist = document.getElementById("tracklist");
   tracklist.innerHTML = "<h2>Tracklist</h2>";
   albumData.tracklist.forEach((track) => {
-    tracklist.innerHTML += `<p>${track.position}. ${track.title} (${track.duration})</p>`;
+    tracklist.innerHTML += `<p><strong>${track.position}.</strong> ${track.title} <small>(${track.duration})</small></p>`;
   });
 
   // SHOW CONTENT DIV
